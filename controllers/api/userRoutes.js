210daddc155ce3190e37d12d0f1574ba3605ea07
Christilato <christilato@gmail.com>
2022-10-13 10:26:11 -0500

    created routes files and pseudo coded what we need to do

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Adding/ Creating a New user
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -16,6 +17,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+//Login route for a user
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -48,6 +50,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+//Logout an existing user
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
@@ -59,3 +62,4 @@ router.post('/logout', (req, res) => {
 });
 
 module.exports = router;
+
