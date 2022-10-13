34c8fb4ae40ed4a9b7a0163f66a7896691445af7
Tyler Esselman <tme91302@gmail.com>
2022-10-13 09:14:12 -0700

    added back in a few routes and confirmed user create/login/logout functionality

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SECRET,
   cookie: {
     maxAge: 300000,
     httpOnly: true,
