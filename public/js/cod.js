8ef96db239e3f20134adab8eed62a5e2e52cf876
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-10-17 13:37:38 -0500

    finished char of the day

--- public/js/cod.js
+++ public/js/cod.js
@@ -1,27 +1,28 @@
-const codEl = $('#codEl');
-
+//const codEl = $('#codEl');
 
 var charOfDay = function () {
     fetch(`api/cod`)
         .then(response => response.json())
         .then(response => {
-            console.log(response)
-           
-                    let characterHtml = `
-                    <h1 class="uk-heading-divider">Character of the Day</h1>
+            console.log(response[0])
+                    let codHtml = `
+                    <h3 class="uk-heading-divider uk-card-title
+                    ">Character of the Day</h1>
                     <div class="uk-card uk-card-default uk-width-1-2@m">
                     <div class="uk-card-header">
                     <div class="uk-grid-small uk-flex-middle" uk-grid>
                     <div class="uk-width-expand">
-                    <img src="${response.imageUrl}" width="900" height="600" alt="">
-                    <h1>fullName: ${response[0].fullName}</h1>
-                    <h2>title: ${response[0].title}</h2>
-                    <h2>family: ${response[0].family}</h2>
+                    <img src="${response.imageUrl}" width="300" height="300" alt="">
+                    <h5>Name: ${response.fullName}</h5>
+                    <h5>Titles: ${response.title}</h5>
+                    <h5>House: ${response.family}</h5>
                     
                     </div>
                     </div>
                     </div>
                     </div>`
-                    $('#character-html').html(characterHtml);
+                    $('#cod-html').html(codHtml);
                 })
         }
+
+charOfDay();
