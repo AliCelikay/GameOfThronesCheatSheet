5963d474be3d5c7dc06a3e37ffb06335c1bf7c4a
Tyler Esselman <tme91302@gmail.com>
2022-10-17 08:34:13 -0700

    cleaned up extra house code

--- public/js/search.js
+++ public/js/search.js
@@ -59,59 +59,6 @@ bookSelectEl.on('change', (event) => {
     bookMatch(bookTitle);
 })
 
-// fetch('/api/houses')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data);
-//         const allHouses = data.map(house => {
-//             const opEl = $('<option>')
-//             opEl.text(house.name);
-//             opEl.val(house.name)
-//             return opEl;
-//         });
-//         console.log(allHouses);
-//         bookSelectEl.append(allHouses)
-//         houseMatch(data);
-//     });
-
-// function houseMatch(houseName) {
-//     // we need to go and fetch the data about this title
-//     console.log(houseName)
-//     fetch(`/api/houses/${houseName}`)
-//         .then(response => response.json())
-//         .then(response => {
-//             console.log(response)
-//             // with the response we render stuff on page
-//             //investigate the response to find the bits of data you definitely want
-//             // use a template literal to be passed as html to the root
-//             // We're getting error on line 34, check w/ instructor
-//             let houseHtml = `<h1 class="uk-heading-divider">Selected House</h1>
-//             <div class="uk-card uk-card-default uk-width-1-2@m">
-//             <div class="uk-card-header">
-//             <div class="uk-grid-small uk-flex-middle" uk-grid>
-//             <div class="uk-width-expand">
-//             <h1>House: ${response[0].name}</h2>
-//             <h2>Region: ${response[0].region}</h2>
-//             <h2>Coat of arms: ${response[0].coatOfArms}</h2>
-//             <h2>Words: ${response[0].words}</h2>
-//             <h2>Associated titles: ${response[0].titles}</h2>
-//             <h2>Seat(s): ${response[0].seats}
-//             </div>
-//             </div>
-//             </div>
-//             </div>`
-//             // $('#root').text(JSON.stringify(response, null, 2))
-//             $('#display-book').html(houseHtml);
-//         })
-// }
-// houseSelectEl.on('change', (event) => {
-//     event.preventDefault();
-//     var houseName = event.target.value
-//     if (houseName === '') return;
-//     bookMatch(houseName);
-// })
-
-
 submitBtn.on("click", function (event) {
     event.preventDefault();
     // .val() grabs the value stored inside the textbox, then we assign the value to the var
